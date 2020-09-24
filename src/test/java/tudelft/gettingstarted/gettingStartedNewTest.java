9b223043e0ec2e9fb9e63b706519093d129e89f4
DorotaBdrn <dorotabiedron@icloud.com>
2020-09-24 21:42:32 +0200

    new test method

--- src/test/java/tudelft/gettingstarted/gettingStartedNewTest.java
+++ src/test/java/tudelft/gettingstarted/gettingStartedNewTest.java
@@ -5,9 +5,15 @@ import org.junit.jupiter.api.Test;
 public class gettingStartedNewTest {
 
     @Test
-    public void addFiveToFiveTest(){
+    public void addFiveToFiveTest() {
         int result = new GettingStarted().addFive(5);
-        Assertions.assertEquals(10,result);
+        Assertions.assertEquals(10, result);
 
+    }
+
+    @Test
+    public void addOneToFiveTest() {
+        int result = new GettingStarted().addFive(1);
+        Assertions.assertEquals(6, result);
     }
 }
